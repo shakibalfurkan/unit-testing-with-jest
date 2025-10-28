c6f4e674fd0137ba81c4974ae6b7c6a9824b0b68
shakibalfurkan <shakibalfurkan@gmail.com>
2025-10-28 18:58:04 +0600

    toEqual, Falsy and Truthy matchers explored

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,24 @@
-import { describe, it, expect } from "@jest/globals";
 import { sum } from "./sum";
 
 describe("sum function", () => {
   it("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("testing falsy values", () => {
+  const n = 0;
+  expect(n).toBeFalsy();
+});
+
+test("testing truthy values", () => {
+  const n = 1;
+  expect(n).toBeTruthy();
 });
